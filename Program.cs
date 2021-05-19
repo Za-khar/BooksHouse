b9c99a871fe2bf15c2aa4adc5ce4b592d4b3c11d
Zakhar <london95vv@gmail.com>
2021-05-19 14:26:44 +0300

    Rename form

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace BooksHouse
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
