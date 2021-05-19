f88bbaace9626662119880665e640465e78d8b75
vdzeer <vdzerniuk@gmail.com>
2021-05-19 15:23:45 +0300

    Create MainForm components

--- MainForm.cs
+++ MainForm.cs
@@ -16,5 +16,30 @@ namespace BooksHouse
         {
             InitializeComponent();
         }
+
+        private void TextBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void TextBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+        private void TextBox3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+        }
+
+        private void TextBox4_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = "";
+        }
+
+        private void TextBox5_Click(object sender, EventArgs e)
+        {
+            textBox5.Text = "";
+        }
     }
 }
