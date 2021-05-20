daa521acb554d72ebb04ff33a18472e6f7519b4a
vdzeer <vdzerniuk@gmail.com>
2021-05-20 11:43:37 +0300

    Add validators and auth

--- EditForm.Designer.cs
+++ EditForm.Designer.cs
@@ -30,8 +30,14 @@ namespace BooksHouse
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -39,26 +45,55 @@ namespace BooksHouse
             this.minAge = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.maxAge = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.task = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.AutoSize = true;
-            this.panel1.BackColor = System.Drawing.SystemColors.Desktop;
-            this.panel1.Location = new System.Drawing.Point(-1, 2);
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1185, 80);
-            this.panel1.TabIndex = 1;
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(476, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 42);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Будинок мрій";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Додати книгу";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(133, 458);
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(200, 523);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 54);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Size = new System.Drawing.Size(156, 37);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Додати";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dataGridView
@@ -75,15 +110,96 @@ namespace BooksHouse
             this.maxAge,
             this.task});
             this.dataGridView.GridColor = System.Drawing.SystemColors.ControlLightLight;
-            this.dataGridView.Location = new System.Drawing.Point(400, 109);
+            this.dataGridView.Location = new System.Drawing.Point(488, 109);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.RowHeadersVisible = false;
             this.dataGridView.RowHeadersWidth = 51;
             this.dataGridView.RowTemplate.Height = 24;
-            this.dataGridView.Size = new System.Drawing.Size(772, 403);
+            this.dataGridView.Size = new System.Drawing.Size(686, 569);
             this.dataGridView.TabIndex = 3;
             this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
             this.dataGridView.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.Location = new System.Drawing.Point(148, 314);
+            this.textBox1.MaximumSize = new System.Drawing.Size(250, 35);
+            this.textBox1.MaxLength = 10;
+            this.textBox1.MinimumSize = new System.Drawing.Size(250, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(250, 35);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "Введіть назву";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.WordWrap = false;
+            this.textBox1.Click += new System.EventHandler(this.TextBox1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox2.Location = new System.Drawing.Point(148, 365);
+            this.textBox2.MaximumSize = new System.Drawing.Size(250, 35);
+            this.textBox2.MaxLength = 10;
+            this.textBox2.MinimumSize = new System.Drawing.Size(250, 35);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(250, 35);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TabStop = false;
+            this.textBox2.Text = "Введіть ціну";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.WordWrap = false;
+            this.textBox2.Click += new System.EventHandler(this.TextBox2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox3.Location = new System.Drawing.Point(148, 416);
+            this.textBox3.MaximumSize = new System.Drawing.Size(250, 35);
+            this.textBox3.MaxLength = 10;
+            this.textBox3.MinimumSize = new System.Drawing.Size(250, 35);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox3.Size = new System.Drawing.Size(250, 35);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TabStop = false;
+            this.textBox3.Text = "Введіть кількість";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.WordWrap = false;
+            this.textBox3.Click += new System.EventHandler(this.TextBox3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox4.Location = new System.Drawing.Point(148, 469);
+            this.textBox4.MaximumSize = new System.Drawing.Size(250, 35);
+            this.textBox4.MaxLength = 10;
+            this.textBox4.MinimumSize = new System.Drawing.Size(250, 35);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox4.Size = new System.Drawing.Size(250, 35);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TabStop = false;
+            this.textBox4.Text = "Введіть вікові межі (через \'-\')";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.WordWrap = false;
+            this.textBox4.Click += new System.EventHandler(this.TextBox4_Click);
             // 
             // id
             // 
@@ -92,7 +208,6 @@ namespace BooksHouse
             this.id.MinimumWidth = 6;
             this.id.Name = "id";
             this.id.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.id.Width = 80;
             // 
             // name
             // 
@@ -101,7 +216,6 @@ namespace BooksHouse
             this.name.MinimumWidth = 6;
             this.name.Name = "name";
             this.name.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.name.Width = 80;
             // 
             // price
             // 
@@ -110,7 +224,6 @@ namespace BooksHouse
             this.price.MinimumWidth = 6;
             this.price.Name = "price";
             this.price.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.price.Width = 80;
             // 
             // amount
             // 
@@ -119,7 +232,6 @@ namespace BooksHouse
             this.amount.MinimumWidth = 6;
             this.amount.Name = "amount";
             this.amount.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.amount.Width = 80;
             // 
             // minAge
             // 
@@ -128,7 +240,6 @@ namespace BooksHouse
             this.minAge.MinimumWidth = 6;
             this.minAge.Name = "minAge";
             this.minAge.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.minAge.Width = 80;
             // 
             // maxAge
             // 
@@ -137,7 +248,6 @@ namespace BooksHouse
             this.maxAge.MinimumWidth = 6;
             this.maxAge.Name = "maxAge";
             this.maxAge.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.maxAge.Width = 80;
             // 
             // task
             // 
@@ -146,24 +256,31 @@ namespace BooksHouse
             this.task.MinimumWidth = 6;
             this.task.Name = "task";
             this.task.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.task.Width = 80;
             // 
             // EditForm
             // 
             this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.ClientSize = new System.Drawing.Size(1184, 778);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximumSize = new System.Drawing.Size(1200, 800);
             this.MinimumSize = new System.Drawing.Size(1200, 800);
             this.Name = "EditForm";
             this.Text = "EditForm";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.EditForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -174,6 +291,12 @@ namespace BooksHouse
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataGridViewTextBoxColumn id;
         private System.Windows.Forms.DataGridViewTextBoxColumn name;
         private System.Windows.Forms.DataGridViewTextBoxColumn price;
